81d601ba9089f60aca95ffa54f9509cac4e7ee65
Natiman58 <natiman58@gmail.com>
2022-07-24 19:53:57 +0300

    Zomies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/**
+ * infinite_while - an infinite loop
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (true)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates the zombie processes
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	pid_t zombie_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+		if (!zombie_pid)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie_pid);
+	}
+
+	infinite_while();
+	return (0);
+}
+
