2feed08783a21225d8d40d7fa9bfbab162bdf7d9
natiman58 <natiman58@gmail.com>
2022-10-17 13:21:35 +0300

    gather

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    A python script that gathers API information, using REST API; url
+"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    name = response.json().get('name')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+
+    total_tasks = []
+    completed = 0
+
+    for task in tasks:
+        if task.get('completed'):
+            total_tasks.append(task)
+            completed += 1
+
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(name, completed, len(tasks)))
+
+    for task in total_tasks:
+        print("\t {}".format(task.get('title')))
